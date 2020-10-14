f0f8316cf980640a3a768b6f7b26cf28482ae5ed
PhroggDev <55362644+PhroggDev@users.noreply.github.com>
2020-10-14 13:52:06 -0400

    Add files via upload
    Trying to publish these on the github-pages feature

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+// const myHeading = document.querySelector('h1');
+let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello World!';
+let myButton = document.querySelector('button');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  }
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
